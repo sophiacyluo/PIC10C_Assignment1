ff19172773c1aec8722f7111eb05ed5de5c2cfea
Sophia Luo <sophialuo410@gmail.com>
2018-06-17 23:32:51 -0700

    Created Xcode project

---
+++ PIC10C_Assignment1/PIC10C_Assignment1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PIC10C_Assignment1
+//
+//  Created by Sophia Luo on 6/17/18.
+//  Copyright © 2018 Sophia Luo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
