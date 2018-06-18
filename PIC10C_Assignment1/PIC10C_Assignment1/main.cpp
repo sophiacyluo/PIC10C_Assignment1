368d4fa17f3ac07670e5f52f0431615648d7e516
Sophia Luo <sophialuo410@gmail.com>
2018-06-17 23:53:18 -0700

    Created an interactive menu

--- PIC10C_Assignment1/PIC10C_Assignment1/main.cpp
+++ PIC10C_Assignment1/PIC10C_Assignment1/main.cpp
@@ -1,15 +1,73 @@
-//
-//  main.cpp
-//  PIC10C_Assignment1
-//
-//  Created by Sophia Luo on 6/17/18.
-//  Copyright © 2018 Sophia Luo. All rights reserved.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+void game_start();
+void print_log();
+void error_message();
+
+int main()
+{
+    bool more = true;
+    
+    while(more)
+    {
+        cout << "-----------Menu-----------" << endl;
+        cout << "Start a new game (press 1)" << endl;
+        cout << "View history (press 2)" << endl;
+        cout << "Exit (press 3)" << endl;
+        
+        int answer = 0;
+        cin >> answer;
+        
+        if(answer == 1)
+            game_start();
+        else if(answer == 2)
+            print_log();
+        else if(answer == 3)
+            more = false;
+        else
+        {
+            more = false;
+            error_message();
+        }
+    }
+    
     return 0;
+}
+
+void game_start()
+{
+    cout << "The game has started." << endl;
+}
+
+void print_log()
+{
+    cout << "Print log" << endl;
+}
+
+void error_message()
+{
+    bool more = true;
+    
+    while(more)
+    {
+        cout << "There has been an error. Please make a valid choice" << endl;
+        
+        int answer = 0;
+        cin >> answer;
+        
+        if(answer == 1)
+        {
+            more = false;
+            game_start();
+        }
+        else if(answer == 2)
+        {
+            more = false;
+            print_log();
+        }
+        else if(answer == 3)
+            more = false;
+    }
 }
